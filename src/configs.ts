73e271c498b47d218e67b1c52245f7d7d84e52ed
mojtaba eslami <mojtabaeslami.72@gmail.com>
2023-05-02 18:50:05 +0330

    add bot_token guard

--- src/configs.ts
+++ src/configs.ts
@@ -7,23 +7,32 @@ export interface Configs {
   autoTrunc: boolean;
   getUserDevicesFromEmail: boolean;
   userDevices?: number;
-  botToken: string | undefined;
+  botToken: string;
 }
 
-const {
-  DELAY,
-  XRAY_LOG_FILE_PATH,
-  AUTO_TRUNC,
-  GET_USER_DEVICES_FROM_EMAIL,
-  USER_DEVICES,
-  BOT_TOKEN,
-} = process.env;
+function createConfig(): Configs {
+  const {
+    DELAY,
+    XRAY_LOG_FILE_PATH,
+    AUTO_TRUNC,
+    GET_USER_DEVICES_FROM_EMAIL,
+    USER_DEVICES,
+    BOT_TOKEN,
+  } = process.env;
 
-export const configs: Configs = {
-  delay: Number(DELAY) ?? 5000,
-  xrayLogFilePath: XRAY_LOG_FILE_PATH ?? './access.log',
-  autoTrunc: Boolean(AUTO_TRUNC) ?? false,
-  getUserDevicesFromEmail: Boolean(GET_USER_DEVICES_FROM_EMAIL) ?? false,
-  userDevices: Number(USER_DEVICES) ?? 2,
-  botToken: BOT_TOKEN,
-};
+  const configs: Configs = {
+    delay: Number(DELAY) ?? 5000,
+    xrayLogFilePath: XRAY_LOG_FILE_PATH ?? './access.log',
+    autoTrunc: Boolean(AUTO_TRUNC) ?? false,
+    getUserDevicesFromEmail: Boolean(GET_USER_DEVICES_FROM_EMAIL) ?? false,
+    userDevices: Number(USER_DEVICES) ?? 2,
+    botToken: BOT_TOKEN as string,
+  };
+
+  if (configs.botToken === '')
+    throw new Error('"BOT_TOKEN" env var is required!');
+
+  return configs;
+}
+
+export default createConfig();
